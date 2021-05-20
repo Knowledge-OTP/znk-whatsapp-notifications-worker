675b1b881949660636be227eec421516072b8e60
Japsz <benjamin.meneses.14@sansano.usm.cl>
2021-05-20 18:43:59 -0400

    Slack message when ignoring whatsapp message

--- processors/messageProcessor.js
+++ processors/messageProcessor.js
@@ -1,5 +1,7 @@
 const templateModel = require('../utils/templater')
 const frontAppService = require('../utils/frontAdapter')
+const slackService = require('../utils/slack')
+
 module.exports = async function(job){
   const {templateKey, recipients, arguments} = job.data
   const templateInfo = await templateModel.findOne(templateKey)
@@ -11,7 +13,7 @@ module.exports = async function(job){
     console.log('to:')
     console.log({recipients})
     console.log('--0---')
-    return shouldSend ? frontAppService.sendMessage(message, recipients) : false
+    return shouldSend ? frontAppService.sendMessage(message, recipients) : slackService.sendMessage(`Ignoring message to ${recipients.join(', ')} \`\`\`${message}\`\`\``)
   } else {
     console.log('no Template Found')
   }
