a0bd4ffe4aa171982f2e5eaeeca19e1a981d3bb6
Richar Lujano <richarj.lujano@gmail.com>
2022-07-13 12:39:23 -0400

    debug whatsap errors

--- processors/messageProcessor.js
+++ processors/messageProcessor.js
@@ -3,19 +3,26 @@ const frontAppService = require('../utils/frontAdapter')
 const slackService = require('../utils/slack')
 
 module.exports = async function(job){
-  const {templateKey, recipients, arguments} = job.data
-  const templateInfo = await templateModel.findOne(templateKey)
-  const shouldSend = process.env.ENABLE_SENDING
-  if(templateInfo){
-    const message = templateModel.render(templateInfo.message, arguments)
-    console.log('--0---')
-    console.log(`About to send message: ${message}`)
-    console.log('to:')
-    console.log({recipients})
-    console.log('--0---')
-    return shouldSend ? frontAppService.sendMessage(message, recipients) : slackService.sendMessage(`Ignoring message to ${recipients.join(', ')} \`\`\`${message}\`\`\``)
-  } else {
-    console.log('no Template Found')
+  try {
+    const {templateKey, recipients, arguments} = job.data
+    const templateInfo = await templateModel.findOne(templateKey)
+    const shouldSend = process.env.ENABLE_SENDING
+    if(templateInfo){
+      const message = templateModel.render(templateInfo.message, arguments)
+      console.log('--0---')
+      console.log(`About to send message: ${message}`)
+      console.log('to:')
+      console.log({recipients})
+      console.log('--0---')
+      return shouldSend ? frontAppService.sendMessage(message, recipients) : slackService.sendMessage(`Ignoring message to ${recipients.join(', ')} \`\`\`${message}\`\`\``)
+    } else {
+      console.log('no Template Found')
+    }
+  } catch (error) {
+    const {templateKey, recipients} = job.data
+    slackService.sendMessage(`Catch error send whatsapp -- ${templateKey} -- message: to ${recipients.join(', ')} \`\`\`${JSON.stringify(error)}\`\`\``)
+    return false
   }
+  
   return false
 }
