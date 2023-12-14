e8ddd1c21aded6684850eee3b985200af194e147
Yilber Rodriguez <117371588+yarodriguez1994@users.noreply.github.com>
2023-12-14 17:08:36 -0500

    feat: added bcc mail zinkerz

--- utils/mandrill.js
+++ utils/mandrill.js
@@ -9,7 +9,10 @@ module.exports = {
         }
         const emailTemplateName = mailOptions.templateName
         console.log(`About to send template: ${emailTemplateName}, Recipients: ${JSON.stringify(mailOptions.emails)}`)
-        const recipientEmails = (mailOptions.emails || []).map((email) => ({email, type: 'to'}))
+        const recipientEmails = (mailOptions.emails || []).map((email) => ({
+            email, 
+            type: email === 'dev+liveprepLogs@zinkerz.com' ? 'bcc' : 'to'
+        }))
         const message = {
             global_merge_vars: mailOptions.params,
             subject: mailOptions.subject,
